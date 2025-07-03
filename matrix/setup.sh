cbd6cc9c6a8fae0d2c0eb4f7aa166d4861e6398f
Fedor Garanin <yesfedor.go@gmail.com>
2025-07-03 23:49:30 +0300

    Matrix fix.23

---
+++ matrix/setup.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+git checkout undo --force
+git checkout master --force
+
+docker-compose down -v
+rm -rf ./synapse-data/* ./postgres-data/*
+
+mkdir -p ./synapse-data/{media_store,uploads,logs}
+chmod -R 777 ./synapse-data  # Временное решение для root
+
+docker-compose run --rm synapse generate
+
+nano synapse-data/homeserver.yaml
+
+# docker-compose up -d
